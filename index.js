38cdeccc158a4048c0abed1a9877523b0a3148d7
Shardul Negi <shardulnegi12@gmail.com>
2020-07-19 03:02:09 +0530

    implemented tree structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const $ = require('jquery');
 require('jstree');
 const nodePath = require('path');
+const fs = require('fs');
 
 $(document).ready(function(){
 
@@ -13,6 +14,10 @@ $(document).ready(function(){
         parent:'#',
         text: getNamefromPath(currpath)
     }
+
+    let rootChildren = getCurrentDirectories(currpath);
+    data = data.concat(rootChildren);
+
     data.push(baseobj);
 
     $('#jstree').jstree({
@@ -20,9 +25,37 @@ $(document).ready(function(){
             "check_callback": true,
             "data":data
         }
-    })
+    }).on('open_node.jstree', function(e, data) {
+        // console.log(data.node.children);
+
+        data.node.children.forEach(function(child) {
+            let childDirectories = getCurrentDirectories(child);
+            childDirectories.forEach(function(directory){
+               $('#jstree').jstree().create_node(child,directory,'last') 
+            })
+        });
+    });
 })
 
 function getNamefromPath(path){
     return nodePath.basename(path);
+}
+
+function getCurrentDirectories(path){
+   if(fs.lstatSync(path).isFile()){
+        return [];
+    }
+    let files = fs.readdirSync(path);
+    console.log(files);
+
+    let rv = [];
+    for(let i=0;i<files.length;i++){
+        let file = files[i];
+        rv.push({
+            id:nodePath.join(path,file),
+            parent:path,
+            text:file
+        })
+    }
+    return rv;
 }
